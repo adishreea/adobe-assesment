4b44273c97c0096b05abfa6dc93cb09858ba173f
Adishree Annabathula <adishreea@Chaitanyas-MacBook-Pro-2.local>
2023-12-02 19:54:59 -0700

    feat(leads): dedup leads and print logs

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import * as fs from 'fs';
+import { DataType, ObjKeyType } from './types';
+
+console.log('\nParsing the leads JSON file');
+const rawData = fs.readFileSync('src/input/leads.json', 'utf-8');
+const jsonData = JSON.parse(rawData);
+
+const dataMap = new Map<ObjKeyType, DataType>();
+
+jsonData.leads?.forEach((curObj: DataType) => {
+  console.log(`\nCurrent record => ${JSON.stringify(curObj)}`);
+
+  const objKey: ObjKeyType = { id: curObj._id, email: curObj.email };
+  const dupObj = findDuplicate(dataMap, objKey);
+  if (dupObj) {
+    console.log('Status: Duplicate');
+    console.log(`Existing record => ${JSON.stringify(dupObj)}`);
+
+    const dupObjDate: Date = new Date(dupObj.entryDate);
+    const curObjDate: Date = new Date(curObj.entryDate);
+    if (curObjDate >= dupObjDate) {
+      console.log('Replacing with the latest record');
+      dataMap.set(objKey, curObj);
+    }
+  } else {
+    console.log('Status: Unique');
+    dataMap.set(objKey, curObj);
+  }
+});
+
+console.log('\n***** OUTPUT *****');
+dataMap.forEach((x) => {
+  console.log(JSON.stringify(x));
+});
+
+function findDuplicate(
+  map: Map<ObjKeyType, DataType>,
+  key: ObjKeyType,
+): DataType {
+  for (const entry of map.entries()) {
+    // check if id or email is a duplicate
+    if (entry[0].id === key.id || entry[0].email === key.email) {
+      return entry[1];
+    }
+  }
+}
---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export type DataType = {
+  _id: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  address: string;
+  entryDate: string;
+};
+
+export type ObjKeyType = {
+  id: string;
+  email: string;
+};
